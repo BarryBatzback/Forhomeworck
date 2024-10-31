3b09b128e031cddff453bfa1d86bbb9b28de2011
thyar <thyartismurder1711@gmail.com>
2024-10-31 21:28:31 +0300

    Практическая работа по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Сергей'
+print ('Name: ' + (str(name)))
+age = 31
+print ('Age: ' + (str(age)))
+New_age = 32
+print ('New Age: ' + (str(New_age)))
+Is_student = True
+print ('Is Student: ' + (str(Is_student)))
