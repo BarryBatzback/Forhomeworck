9a03fc68dd13b15299824ab3f28b401e8e369a87
thyar <thyartismurder1711@gmail.com>
2024-11-01 21:01:29 +0300

    Практическое задание по теме "Переменные"ÑÑ

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+name_kurs = 'Python'
+HW_end = 12
+Hours_spent = 1.5
+TPT = (Hours_spent / HW_end)
+print ('Курс: ' + (str(name_kurs)) + ',', 'Всего задач: ' + (str(HW_end)) + ',', 'Затрачено часов: ' + (str(Hours_spent)) + ',', 'Среднее время выполнения одного задания: ' + (str(TPT)) + ' часа.')
